3a3bb00ba35998d8c51d39745ea19f0dea430200
Li-Lingxi <103548589+Li-Lingxi@users.noreply.github.com>
2025-09-14 09:54:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.getElementById("helloBtn");
+  btn.addEventListener("click", function () {
+    alert("你好！网站已经运行啦 🚀");
+  });
+});
